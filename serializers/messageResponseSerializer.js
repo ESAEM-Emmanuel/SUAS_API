c3700d361f8a57137c440c2f619ab6156e80c292
ESAEM-Emmanuel <emmanuelessimagel@gmail.com>
2024-09-03 14:27:30 +0100

    ajout de tags au messages

--- serializers/messageResponseSerializer.js
+++ serializers/messageResponseSerializer.js
@@ -3,6 +3,7 @@ const messageResponseSerializer = (message) => ({
     referenceNumber: message.referenceNumber,
     workshopId: message.workshopId,
     content: message.content,
+    tag: message.tag,
     urlFile: message.urlFile,
     messageType: message.messageType,
     participantId: message.participantId,
